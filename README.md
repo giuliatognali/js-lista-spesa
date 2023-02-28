80e7acac1814d028007f6bd72b5bab28c56347cf
giuliatognali <giuliatognali@gmail.com>
2023-03-01 00:17:22 +0100

    input not working

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ Creare una lista della spesa usando il ciclo ```while```.
 3. Stampo in console 
 
 4. Creo nell'HTML l'elemento ul a cui verranno appesi gli elementi della lista.
+
+BONUS
+
+Aggiungiamo un elemento alla lista
+1. Creo una variabile bottone e un input
+
+2. All'evento del bottone verrà aggiunto il nuovo valore inserito nell'input.
+    Se il valore di input non è vuoto quindi è !=='', push il nuovo valore nella lista.
--- js/main.js
+++ js/main.js
@@ -4,12 +4,36 @@ const shoppingList = ['dentifricio', 'cocacola', 'vitamine', 'lenticchie', 'caff
 
 const ul = document.getElementById('shopping-list');
 
-
 let i = 0;
-while(i < shoppingList.length){
-    console.log(shoppingList[i]);
-    const itemList =document.createElement('li');
+while (i < shoppingList.length) {
+    //console.log(shoppingList[i]);
+    const itemList = document.createElement('li');
     itemList.append(shoppingList[i]);
     ul.append(itemList);
     i++;
 }
+
+//variabili per aggiungere elementi alla lista
+const inputAdd = document.getElementById('add-to-list');
+const addBtn = document.getElementById('add-btn');
+const clearBtn = document.getElementById('clear-btn');
+
+let newItem = '';
+
+addBtn.addEventListener('click',
+    function () {
+        if (inputAdd.valute !=''){
+            let inputValue = inputAdd.value;
+            shoppingList.push(inputValue);
+            newItem = document.createElement('li');
+            
+            newItem.innterHTML = inputValue;
+            newItem.append(shoppingList[i]);
+            ul.append(newItem);
+            
+             
+
+            console.log(newItem);
+        }
+    }
+)
